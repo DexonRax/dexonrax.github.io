8a1688b92f4ea3870e2c0c69a42997063223dffd
Dexon Rax <dexonrax@gmail.com>
2023-12-19 22:12:06 +0100

    metal font gen rewritten

--- metal/script.js
+++ metal/script.js
@@ -5,7 +5,27 @@ function reverseString(str) {
 	    return joinArray;
 	}
 
-function text_changed(){
+function FormatText(str){
+	let input_text = document.getElementById("input_text")
+	let text_length = input_text.value.length
+	let text_until_flip = Math.ceil(text_length / 2)
+	let output_h1 = ""
+	let output_h2 = ""
+	let output_final = []
+	for(let i = 0; i<text_length; i++){
+		if(i<text_until_flip){
+			output_h1 += input_text.value[i]
+		}
+		else if(i>=text_until_flip){
+			output_h2 += input_text.value[i]
+		}
+	}
+
+	output_final = [output_h1, reverseString(output_h2)]
+	return output_final
+}
+
+/*function text_changed(){
 	let input_text = document.getElementById("input_text")
 	let text_length = input_text.value.length
 	let text_until_flip = Math.ceil(text_length / 2)
@@ -22,19 +42,34 @@ function text_changed(){
 		}
 	}
 	document.getElementById("text_edit_right").innerHTML = reverseString(document.getElementById("text_edit_right").innerHTML);
-}
+}*/
 
-function change_font(){
-	document.getElementById("text_edit_left").style.fontFamily = document.getElementById("font_select").value
-	document.getElementById("text_edit_right").style.fontFamily = document.getElementById("font_select").value
-}
+function text_changed(){
+	const canvas = document.getElementById("image");
+	const ctx = canvas.getContext("2d");
+	var scale = window.devicePixelRatio;     
+    canvas.width = Math.floor(512 * scale); 
+    canvas.height = Math.floor(512 * scale); 
 
-function change_spacing(){
-	document.getElementById("text_edit_left").style.letterSpacing = document.getElementById("letter_spacing").value
-	document.getElementById("text_edit_right").style.letterSpacing = document.getElementById("letter_spacing").value
-}
+	ctx.clearRect(0, 0, 512, 512);
+	ctx.fillStyle = "black";
+	ctx.fillRect(0, 0, 512, 512);
 
-function font_size(){
-	document.getElementById("text_edit_left").style.fontSize = document.getElementById("font_size").value
-	document.getElementById("text_edit_right").style.fontSize = document.getElementById("font_size").value
+	//alert(document.getElementById("font_size").value+" "+document.getElementById("font_select").value)
+
+	ctx.fillStyle = "white";
+	ctx.textAlign = "center";
+	ctx.font = document.getElementById("font_size").value+" "+document.getElementById("font_select").value
+
+    var centerX = canvas.width / 2;
+    var centerY = canvas.height / 2;
+    var textWidth = ctx.measureText(document.getElementById("input_text").value).width;
+    var nonFlippedX = centerX - (textWidth / 4);
+    var flippedX = centerX + (textWidth / 4);
+
+	ctx.fillText(FormatText(document.getElementById("input_text").value)[0], nonFlippedX, centerY);
+    ctx.scale(-1, 1);
+    ctx.fillText(FormatText(document.getElementById("input_text").value)[1], -flippedX, centerY);
+    ctx.scale(-1, 1);
+
 }
